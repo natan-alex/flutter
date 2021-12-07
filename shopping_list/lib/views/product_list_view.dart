943ed069ca6cca67863b86682717e8e5e9806598
natan-alex <natancarmo321@gmail.com>
2021-12-06 21:17:25 -0300

    show the list items in a better way

--- shopping_list/lib/views/product_list_view.dart
+++ shopping_list/lib/views/product_list_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_list/controllers/product_controller.dart';
+import 'package:shopping_list/models/product.dart';
 
 class ProductListView extends StatefulWidget {
   @override
@@ -14,30 +15,64 @@ class _ProductListViewState extends State<ProductListView> {
     var controller = Provider.of<ProductController>(context);
 
     return Column(children: [
-      PreferredSize(
-          preferredSize: const Size.fromHeight(2),
-          child: AppBar(
-            title: const Text(
-              "Produtos:",
-              style: TextStyle(fontSize: 15),
-            ),
-            centerTitle: true,
-            elevation: 0.0,
-          )),
+      AppBar(
+        title: const Text(
+          "Produtos:",
+          style: TextStyle(fontSize: 15),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
       Observer(
           builder: (_) => controller.products.isNotEmpty
               ? ListView.builder(
                   shrinkWrap: true,
                   itemCount: controller.products.length,
-                  itemBuilder: (_, index) {
-                    final currentItem = controller.products[index];
-
-                    return ListTile(
-                      title: Text(currentItem.name),
-                      subtitle: Text(currentItem.description),
-                    );
-                  })
-              : const Text('Não há nenhum produto para ser exibido'))
+                  itemBuilder: (_, index) => getExpansionTileWithProductInfos(
+                      controller.products[index], controller))
+              : const Padding(
+                  child: Text('Não há nenhum produto para ser exibido'),
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                ))
     ]);
+  }
+
+  getExpansionTileWithProductInfos(
+      Product product, ProductController controller) {
+    return Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Nome do produto: '),
+                    Text(product.name),
+                  ])),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Descrição do produto: '),
+                    Text(product.description)
+                  ])),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Produto já comprado? '),
+                    Checkbox(
+                        value: product.wasAlreadyBought,
+                        onChanged: (value) {
+                          if (value != null) {
+                            product.wasAlreadyBought = value;
+                            controller.updateProduct(product);
+                          }
+                        })
+                  ]))
+        ]));
   }
 }
